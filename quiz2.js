024550d8312672b324617e493b2460fc480872b7
kobebryant60308 <80386886+kobebryant60308@users.noreply.github.com>
2021-03-31 22:24:23 +0800

    Update quiz2.js

--- quiz2.js
+++ quiz2.js
@@ -1,12 +1,12 @@
-var tags = "§õª¾®¦";
-var dataUrl = "https://api.flickr.com/services/feeds/photos\_public.gne?tags=" + tags
+var tags = "æçŸ¥æ©";
+var dataUrl = "https://api.flickr.com/services/feeds/photos_public.gne?tags=" + tags
 +"&tagmode=any&format=json&per_page=400&jsoncallback=?";
 var data = $.getJSON(dataUrl);
 data.done( function( msg ) {
-  $.each(msg.items,function(i,item){
-    $("#contain").html;
-    $("#contain").append($("<img/>").attr("src",item.media.m));
-  });
+$.each(msg.items,function(i,item){
+$("#contain").html;
+$("#contain").append($("<img/>").attr("src",item.media.m));
+});
 });
 data.fail( function( msg ) {
 console.log(msg);
